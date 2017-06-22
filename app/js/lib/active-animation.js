3c1d74d0767ae8c5cf23ac968b0165644afe6a4c
developer zenman <kellee@zenman.com>
2017-06-22 14:40:08 -0600

    Optimized images

--- app/js/lib/active-animation.js
+++ app/js/lib/active-animation.js
@@ -2,6 +2,7 @@ var activateAnimation = function() {
     var $easeElement = $(".js-line-animation, .js-ease, .js-from-left");
     $easeElement.each(function(){
         var $this = $(this);
+        // $this.addClass('active');
         $this.bind('inview', function (event, visible) {
             var $that = $(this);
             if (visible === true && !$that.hasClass('active')) {
@@ -12,6 +13,27 @@ var activateAnimation = function() {
         });
     });
 };
+
+var scale = function() {
+    var $easeElement = $(".js-scale");
+    $easeElement.each(function(){
+        var $this = $(this);
+        //$this.addClass('active');
+        $this.bind('inview', function (event, visible) {
+            var $that = $(this);
+            if (visible === true && !$that.hasClass('active')) {
+                $that.addClass('active');
+            } else {
+                return false;
+            }
+        });
+    });
+};
+
 jQuery(function($){
+    $(function () {
+        $(window).scroll();
+    });
     activateAnimation();
+    scale();
 });
